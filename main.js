f6f98bc9f462f280f6c49c4591b4c2ab09ad7922
haunteD7 <228pacan228aue@gmail.com>
2025-10-12 22:05:32 +0300

    do some refactoring

--- main.js
+++ main.js
@@ -1,40 +1,90 @@
 const https = require("https")
-const readline = require('readline')
+const readline = require("readline")
 const open = require('open').default
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+class WikipediaClient {
+    constructor() {
+        this.base_URL = "ru.wikipedia.org"
+    }
 
-rl.question('Введите запрос: ', (article) => {
-    const options = {
-        hostname: `ru.wikipedia.org`,
-        path: encodeURI(`/w/api.php?action=query&list=search&utf8=&format=json&srsearch="${article}"`),
-        headers: {
-            'User-Agent': 'MyCustomApp/1.0 (Node.js HTTPS Client)'
+    find_articles(article) {
+        return new Promise((resolve, reject) => {
+            const options = {
+                hostname: this.base_URL,
+                path: encodeURI(`/w/api.php?action=query&list=search&utf8=&format=json&srsearch="${article}"`),
+                headers: {
+                    'User-Agent': 'MyCustomApp/1.0 (Node.js HTTPS Client)'
+                }
+            }
+            const req = https.get(options, (response) => {
+                let data = ''
+
+                response.on('data', (chunk) => {
+                    data += chunk
+                })
+                response.on('end', () => {
+                    try {
+                        const data_JSON = JSON.parse(data)
+                        resolve(data_JSON)
+                    }
+                    catch(error) {
+                        reject(new Error('Ошибка парсинга JSON: ' + error.message))
+                    }
+                })
+            })
+
+            req.on('error', (error) => {
+                reject(new Error('Ошибка запроса: ' + error.message))
+            })
+
+            req.end()
+        })
+    }
+    extract_article_id(search_data) {
+        try {
+            return search_data.query.search['0'].pageid
+        }
+        catch(error) {
+            throw new Error('Статья не найдена')
         }
     }
-    const req = https.get(options, (response) => {
-        let data = '';
-        response.on('data', (chunk) => {
-            data += chunk
+}
+
+class WikipediaApp {
+    constructor() {
+        this.client = new WikipediaClient
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
         })
-
-        response.on('end', () => {
-            data_json = JSON.parse(data)
-
-            let pageid = 0
-            try {
-                pageid = data_json.query.search['0'].pageid
-            } catch (error) {
-               console.log('Статья не найдена')
-               return 
-            }
-
-            open(`https://ru.wikipedia.org/w/index.php?curid=${pageid}`)
+    }
+    get_input(question) {
+        return new Promise((resolve) => {
+            this.rl.question(question, resolve)
         })
-    })
-    req.on('error', (error) => { console.log("Ошибка запроса: ", error) })
-    rl.close();
-});
+    }
+
+    close() {
+        this.rl.close()
+    }
+    async start() {
+        try {
+            const article = await this.get_input('Поиск статьи: ')
+            const search_data = await this.client.find_articles(article)
+            const page_id = this.client.extract_article_id(search_data)
+            
+            await open(`https://ru.wikipedia.org/w/index.php?curid=${page_id}`)
+
+            console.log('Статья успешно открыта!')
+        }
+        catch (error) {
+            console.log('Ошибка: ' + error.message)
+        }
+        finally {
+            this.close()
+        }
+    }
+}
+
+const app = new WikipediaApp
+app.start()
