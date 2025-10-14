a5c3a21fc1cacface321c65e06fb9b96e8012669
haunteD7 <228pacan228aue@gmail.com>
2025-10-14 20:27:16 +0300

    do a little bit refactoring

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const readline = require("readline")
 const open = require('open').default
 
 class WikipediaClient {
-    constructor() {
-        this.base_URL = "ru.wikipedia.org"
+    constructor(base_URL) {
+        this.#base_URL = base_URL
     }
 
     find_articles(article) {
         return new Promise((resolve, reject) => {
             const options = {
-                hostname: this.base_URL,
+                hostname: this.#base_URL,
                 path: encodeURI(`/w/api.php?action=query&list=search&utf8=&format=json&srsearch="${article}"`),
                 headers: {
                     'User-Agent': 'MyCustomApp/1.0 (Node.js HTTPS Client)'
@@ -48,11 +48,13 @@ class WikipediaClient {
             throw new Error('Статья не найдена')
         }
     }
+
+    #base_URL
 }
 
 class WikipediaApp {
     constructor() {
-        this.client = new WikipediaClient
+        this.client = new WikipediaClient("ru.wikipedia.org")
         this.rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
@@ -84,6 +86,8 @@ class WikipediaApp {
             this.close()
         }
     }
+
+    #client
 }
 
 const app = new WikipediaApp
